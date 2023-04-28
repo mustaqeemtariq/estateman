857ae7b825f3de8a15b2975388071a4247b61d15
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-04-28 16:45:17 +0500

    create next app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import '@components/styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'src/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
