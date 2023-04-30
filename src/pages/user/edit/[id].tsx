41da9249ecac3f0af10971956bc17ecec45a205d
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-04-30 18:46:27 +0500

    add new and edit user page

---
+++ src/pages/user/edit/[id].tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router'
+import UserForm from 'src/components/users/form'
+
+const updateUser = () => {
+	const router = useRouter()
+	const { id } = router.query
+
+	return (
+		<UserForm title='Edit User' isNew={false} userId={id} />
+	)
+}
+
+export default updateUser
+
---
+++ src/pages/user/new.tsx
@@ -0,0 +1,10 @@
+import UserForm from 'src/components/users/form'
+
+const newUser = () => {
+  return (
+    <UserForm title='Add User' isNew={true} />
+  )
+}
+
+export default newUser
+
