2109082b395d5122d3b06016c6d9433665d448c4
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-03 23:09:21 +0500

    design card component

--- src/components/dashboard/card.tsx
+++ src/components/dashboard/card.tsx
@@ -1,23 +1,29 @@
-import { ForwardRefExoticComponent, RefAttributes, SVGProps } from 'react'
+import clsx from 'clsx'
+import Image, { StaticImageData } from 'next/image'
 
-interface HomeCardProps {
+interface DashboardCardProps {
 	name: string
 	stats: number
-	background?: ForwardRefExoticComponent<
-		Omit<SVGProps<SVGSVGElement>, 'ref'> & {
-			title?: string | undefined
-			titleId?: string | undefined
-		} & RefAttributes<SVGSVGElement>
-	>
+	background: StaticImageData
+	count?: number
 }
 
-export const HomeCard = ({ name, stats, background }: HomeCardProps) => {
+export const DashboardCard = ({ name, stats, background, count }: DashboardCardProps) => {
 	return (
 		<div
-			className={`w-[160px] h-[160px] border border-gray-300 flex items-center justify-center bg-[${background}] bg-no-repeat bg-center`}>
-			<div className="mt-2 sm:p-4">
-				<h2 className="text-2xl text-[#27325B] text-center">{stats}</h2>
+			className={`w-[160px] h-[160px] border border-gray-300 flex items-center justify-center relative`}>
+			<div className="mt-2 sm:p-4 space-y-4">
+				<h2
+					className={clsx(
+						'text-5xl font-bold text-[#27325B] text-center',
+						count == 3 && 'text-[#C31E1E]'
+					)}>
+					{stats}
+				</h2>
 				<h2 className="text-lg font-semibold text-gray-900 text-center uppercase">{name}</h2>
+			</div>
+			<div className="absolute bottom-0 right-0">
+				<Image src={background} alt="card" />
 			</div>
 		</div>
 	)
