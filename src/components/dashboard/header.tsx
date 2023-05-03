8532509fafde5134b3b9c1d614428cfd526a28f1
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-03 23:09:39 +0500

    design dashboard header component

---
+++ src/components/dashboard/header.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { Select } from '../app/select'
+import { Tabs } from '../app/tabs'
+
+export const DashboardHeader = () => {
+	const [showTab, setShowTab] = useState('Dashboard')
+
+	const tabs = [
+		{
+			name: 'Dashboard',
+			current: showTab == 'Dashboard' ? true : false
+		}
+	]
+
+	return (
+		<div className="grid grid-cols-2 gap-x-4">
+			<Tabs
+				labelText="Dashboard"
+				name="Dashboard"
+				value="Dashboard"
+				setShowTab={setShowTab}
+				tabs={tabs}
+				className="w-full text-left text-2xl"
+			/>
+			<div className="flex space-x-2">
+				<Select name="period" labelText="SELECT" renderLabel={true}>
+					<option value="">Select Period</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+				</Select>
+				<Select name="period" labelText="FROM" renderLabel={true}>
+					<option value="">Select Period</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+				</Select>
+				<Select name="period" labelText="TO" renderLabel={true}>
+					<option value="">Select Period</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+				</Select>
+			</div>
+		</div>
+	)
+}
