e9ad6c31c9bf5ad633f227620adf37597621b20c
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-03 23:11:28 +0500

    add more enums

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,25 @@
-export const enum UserRightsTypes {
+export const enum UserRightTypes {
 	ADD = 'add_property',
 	EDIT = 'edit_property',
 	VIEW = 'view_property'
+}
+
+export const enum ContractTypes {
+	SALE = 'sale',
+	RENT = 'rent'
+}
+
+export const enum PropertyTypes {
+	RESIDENTIAL = 'residential',
+	COMMERCIAL = 'commercial',
+	SPECIAL = 'special_commercial'
+}
+
+export enum UnitTypes {
+	SQYARD = 'Sq yard',
+	SQFEET = 'Sqft',
+	SQMETER = 'Sq meter',
+	MARLA = 'Marla + Kanal',
+	ACRE = 'Acre',
+	MURABBA = 'Murabba'
 }
