99d17668917f8a4a680e176658bbcb06c64cc037
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-06 15:57:08 +0500

    add new property page

--- src/pages/property/new.tsx
+++ src/pages/property/new.tsx
@@ -1,16 +1,23 @@
+import { useState } from 'react'
 import { Container } from 'src/components/app/container'
 import { AppHeader } from 'src/components/app/header'
 import { AppLayout } from 'src/components/app/layout'
-import PropertyForm from 'src/components/property/add-form'
+import PropertyForm from 'src/components/property/form'
 import { PropertyHeader } from 'src/components/property/header'
 
 const newProperty = () => {
+	const [state, setState] = useState('Add Property')
+	const [active, setActive] = useState({
+		propertyDetails: false,
+		addHistory: false
+	})
+
 	return (
 		<AppLayout>
 			<AppHeader />
 			<Container>
-				<PropertyHeader />
-				<PropertyForm />
+				<PropertyHeader setState={setState} active={active} state={state} />
+				<PropertyForm currentTab={state} setCurrentTab={setState} setActive={setActive} />
 			</Container>
 		</AppLayout>
 	)
