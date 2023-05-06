092851641d6928be47c74ee59432903e2b013c61
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-06 15:51:50 +0500

    design radio component

---
+++ src/components/app/radio.tsx
@@ -0,0 +1,73 @@
+import clsx from 'clsx'
+import { InputHTMLAttributes, useState } from 'react'
+import { FieldError, FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+	labelText?: string
+	name: string
+	index?: string
+	checked?: boolean
+	value?: string
+	register?: UseFormRegister<any>
+	error?: FieldErrors<FieldValues>
+	controllerError?: FieldError | undefined
+	renderLabel?: boolean
+	required?: boolean
+	disabled?: boolean
+	onChange: React.ChangeEventHandler<HTMLInputElement>
+}
+
+export const Radio = ({
+	labelText,
+	index,
+	name,
+	checked,
+	register,
+	disabled,
+	value,
+	error,
+	controllerError,
+	renderLabel = true,
+	required,
+	onChange,
+	...props
+}: InputProps) => {
+	const errorText = (error?.[name]?.message as string) ?? controllerError?.message
+
+	const [checkValue, setCheckValue] = useState<string>()
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { checked } = event.target
+		if (checked) {
+			setCheckValue(value)
+			onChange?.(event)
+		} else {
+			setCheckValue(undefined)
+			onChange?.(event)
+		}
+	}
+
+	return (
+		<div>
+			<div className="mt-1 flex items-center space-x-2">
+				<input
+					{...props}
+					type="radio"
+					name={name}
+					onChange={onChange}
+					id={index}
+					value={value}
+					checked={disabled ? false : checked}
+					disabled={disabled}
+					className={clsx('border border-gray-400')}
+				/>
+				{renderLabel && labelText && (
+					<label htmlFor={name} className={clsx('block text-sm font-small text-[#0D0C18]')}>
+						{labelText}
+					</label>
+				)}
+			</div>
+			{errorText && <p className="text-xs text-red-600">{errorText}</p>}
+		</div>
+	)
+}
