915bc2914b6c6496961a2e47d1a5da53a08180d2
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-06 15:58:59 +0500

    add property form typings

--- src/types/typings.d.ts
+++ src/types/typings.d.ts
@@ -1,4 +1,5 @@
 import { ContractTypes, PropertyTypes, UnitTypes, UserRightTypes } from 'src/constants/constants'
+import { AddPropertyForm } from 'src/types/typings'
 
 interface User {
 	username: string
@@ -15,7 +16,7 @@ type UserForm = User & {
 	rights: UserRightTypes
 }
 
-type PropertyForm = {
+type AddPropertyForm = {
 	title: string
 	contract: ContractTypes
 	property: PropertyTypes
@@ -25,4 +26,50 @@ type PropertyForm = {
 	units: UnitTypes
 	price: string
 	year: number
+}
+
+type OwnerDetails = {
+	name: string
+	phone: string
+	alternatephone: string
+	address: string
+	cnic: string
+	ligitation: string
+	description: string
+}
+
+type PropertyDetailsForm = OwnerDetails & {
+	city: string
+	house: string
+	street?: number
+	society?: string
+	places?: string
+	sector?: string
+	bed?: number
+	bath?: number
+	kitchen?: number
+	gas?: string
+	electricity?: string
+}
+
+type AddHistoryForm = {
+	historydate?: string
+	occupancy?: string
+	leaseexpiry?: string
+	historydetails?: string
+	historyimage?: File[]
+	callrecord?: string
+}
+
+type PropertyFormType = AddPropertyForm & PropertyDetailsForm & AddHistoryForm
+
+type CallRecordForm = {
+	date?: string
+	phone?: string
+	callername?: string
+}
+
+type PricingHistoryForm = {
+	price?: string
+	date?: string
 }
