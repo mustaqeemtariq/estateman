dee1d89803eae9f6c1f56f6cfa3f9562cb2665ff
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-06 15:51:36 +0500

    improve date logic

--- src/components/app/input.tsx
+++ src/components/app/input.tsx
@@ -1,9 +1,12 @@
+import { CalendarDaysIcon } from '@heroicons/react/20/solid'
+import clsx from 'clsx'
 import { InputHTMLAttributes, useState } from 'react'
 import { FieldError, FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form'
 import { containsOnlyDigits } from 'src/utils/string'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	labelText?: string
+	disabled?: boolean
 	name: string
 	index?: string
 	register?: UseFormRegister<any>
@@ -11,6 +14,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 	controllerError?: FieldError | undefined
 	renderLabel?: boolean
 	required?: boolean
+	year?: boolean
 	onChange?: React.ChangeEventHandler<HTMLInputElement>
 }
 
@@ -18,7 +22,10 @@ export const Input = ({
 	labelText,
 	index,
 	name,
+	placeholder,
+	disabled,
 	register,
+	year,
 	error,
 	renderLabel = true,
 	required,
@@ -27,6 +34,24 @@ export const Input = ({
 }: InputProps) => {
 	const errorText = error?.[name]?.message as string
 
+	const [date, setDate] = useState<string>('')
+
+	const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = event.target
+		const date = new Date(value)
+		if (year) {
+			const year = date.getFullYear()
+			setDate(year.toString())
+			onChange?.(event)
+		} else {
+			const formattedDate = `${date.getDate()} ${date.toLocaleString('default', {
+				month: 'short'
+			})}, ${date.getFullYear()}`
+			setDate(formattedDate)
+			onChange?.(event)
+		}
+	}
+
 	return (
 		<div className="w-full">
 			{renderLabel && labelText && (
@@ -34,15 +59,46 @@ export const Input = ({
 					{labelText} {required && <span className="text-[#FF0000]">*</span>}
 				</label>
 			)}
-			<div className="mt-1">
+			<div className="mt-1  relative flex items-center">
 				<input
 					{...props}
 					{...(register?.(name) ?? {})}
-					onChange={onChange}
+					onChange={props.type == 'date' ? handleDateChange : onChange}
+					placeholder={placeholder}
 					id={index}
-					className="block placeholder-[#0D0C18] w-full appearance-none bg-[#E6E6E6] rounded-md border border-gray-300 p-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+					className={clsx(
+						'block placeholder-[#0D0C18] w-full  bg-[#E6E6E6] rounded-md border border-gray-300 p-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm',
+						disabled && 'placeholder-gray-500',
+						props.type == 'date' && 'opacity-0 absolute z-50'
+					)}
 				/>
+				{props.type == 'date' && (
+					<>
+						<input
+							type="text"
+							disabled
+							placeholder={placeholder}
+							value={date}
+							className={clsx(
+								'block placeholder-[#0D0C18] w-full  bg-[#E6E6E6] rounded-md border border-gray-300 p-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm',
+								disabled && 'placeholder-gray-500'
+							)}
+						/>
+						<div
+							className="absolute right-1"
+							onClick={() => {
+								const dateInput = document.querySelector('input[type="date"]') as HTMLInputElement
+								dateInput.click()
+							}}>
+							<CalendarDaysIcon
+								className="h-5 w-5 stroke-black cursor-pointer"
+								aria-hidden="true"
+							/>
+						</div>
+					</>
+				)}
 			</div>
+
 			{errorText && <p className="text-xs text-red-600">{errorText}</p>}
 		</div>
 	)
@@ -54,8 +110,10 @@ export const InputNumber = ({
 	name,
 	error,
 	maxLength,
+	disabled,
 	renderLabel = true,
 	onChange,
+	className,
 	value,
 	required,
 	...props
@@ -78,7 +136,7 @@ export const InputNumber = ({
 	return (
 		<div className="w-full">
 			{renderLabel && labelText && (
-				<label htmlFor={name} className="block text-[#0D0C18]">
+				<label htmlFor={name} className="block text-[#0D0C18] capitalize">
 					{labelText} {required && <span style={{ color: 'red' }}>*</span>}
 				</label>
 			)}
@@ -89,7 +147,11 @@ export const InputNumber = ({
 					value={input}
 					type="text"
 					id={index}
-					className="block placeholder-[#0D0C18] w-full appearance-none bg-[#E6E6E6] rounded-md border border-gray-300 p-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+					className={clsx(
+						'block placeholder-[#0D0C18] w-full appearance-none bg-[#E6E6E6] rounded-md border border-gray-300 p-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm',
+						className,
+						disabled && 'placeholder-gray-500'
+					)}
 				/>
 			</div>
 			{errorText && <p className="text-xs text-red-600">{errorText}</p>}
