27e34ac64a5ce3355f290e90076e4468f1e6a6e6
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-06 15:52:38 +0500

    improve tab logic

--- src/components/app/tabs.tsx
+++ src/components/app/tabs.tsx
@@ -13,16 +13,28 @@ interface TabsProps {
 	setShowTab: Dispatch<SetStateAction<string>>
 	tabs: Tab[]
 	className?: string
+	active: {
+		propertyDetails: boolean
+		addHistory: boolean
+	}
 }
 
-export const Tabs = ({ labelText, name, value, tabs, setShowTab, className }: TabsProps) => {
+export const Tabs = ({
+	labelText,
+	name,
+	value,
+	tabs,
+	setShowTab,
+	className,
+	active
+}: TabsProps) => {
 	return (
 		<div className="w-full">
 			<div className="sm:hidden">
 				<label htmlFor={name} className="sr-only">
 					{labelText}
 				</label>
-				<select id={name} name={name} value={value}>
+				<select id={name} name={name} defaultValue={value}>
 					{tabs.map(tab => (
 						<option key={tab.name}>{tab.name}</option>
 					))}
@@ -31,16 +43,19 @@ export const Tabs = ({ labelText, name, value, tabs, setShowTab, className }: Ta
 			<div className="hidden sm:block">
 				<div>
 					<nav className="-mb-px flex border-b border-gray-400" aria-label="Tabs">
-						{tabs.map(tab => (
+						{tabs.map((tab, index) => (
 							<button
 								onClick={() => {
-									setShowTab(tab.name)
+									index == 0 && setShowTab(tab.name)
+									index == 1 && active.propertyDetails && setShowTab(tab.name)
+									index == 2 && active.addHistory && setShowTab(tab.name)
 								}}
 								key={tab.name}
 								className={clsx(
 									tab.current
-										? 'border-indigo-500 text-gray-900'
-										: 'border-transparent hover:border-gray-300 text-gray-400 hover:text-gray-700 text-lg font-small',
+										? 'border-indigo-500 text-gray-900 text-2xl'
+										: 'border-transparent text-gray-400 text-lg',
+									'hoverborder-gray-300 hover:text-gray-700 font-small',
 									'whitespace-nowrap border-b-4 py-4 px-6 font-medium uppercase ',
 									className
 								)}
