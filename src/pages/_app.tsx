11f96322fe86750934515926821d1cd6180c40b8
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-10 03:30:19 +0500

    use store and toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import { store } from 'src/store'
 import 'src/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<Provider store={store}>
+			<Toaster
+					position={'top-right'}
+					toastOptions={{
+						style: {
+							margin: '15px',
+							background: '#828282',
+							color: '#fff',
+							width: '340px'
+						},
+						className: 'text-base',
+						duration: 3000
+					}}
+				/>
+			<Component {...pageProps} />
+		</Provider>
+	)
 }
