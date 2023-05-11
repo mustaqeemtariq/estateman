13d2dacee993f9dc96f9fbc92e67b420ee78e48f
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-12 03:36:19 +0500

    refactor code

--- src/pages/auction/new.tsx
+++ src/pages/auction/new.tsx
@@ -1,19 +1,21 @@
+import { useState } from 'react'
 import { Container } from 'src/components/app/container'
 import { AppHeader } from 'src/components/app/header'
 import { AppLayout } from 'src/components/app/layout'
 import AuctionForm from 'src/components/auction/form'
 import { AuctionHeader } from 'src/components/auction/header'
 
-const newProperty = () => {
+const newAuction = () => {
+	const [state, setState] = useState('Add Auction')
 	return (
 		<AppLayout>
 			<AppHeader />
 			<Container>
-				<AuctionHeader />
+				<AuctionHeader state={state} setState={setState} />
 				<AuctionForm />
 			</Container>
 		</AppLayout>
 	)
 }
 
-export default newProperty
+export default newAuction
