6a82612fa7d914bc302fcc2df85cf0a6bc19eabf
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-15 02:51:01 +0500

    added protected routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
-import { store } from 'src/store'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import store from 'src/store'
 import 'src/styles/globals.css'
+import Protection from './protection'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
+	let persistor = persistStore(store)
+
 	return (
 		<Provider store={store}>
-			<Toaster
+			<PersistGate persistor={persistor}>
+				<Toaster
 					position={'top-right'}
 					toastOptions={{
 						style: {
@@ -20,7 +26,8 @@ export default function App({ Component, pageProps }: AppProps) {
 						duration: 3000
 					}}
 				/>
-			<Component {...pageProps} />
+				<Protection Component={Component} pageProps={pageProps} router={router} />
+			</PersistGate>
 		</Provider>
 	)
 }
