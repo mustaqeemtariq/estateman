32e2f042e8801478e50e2123a3545787ef931b5c
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-15 02:53:28 +0500

    add protected routes

---
+++ src/pages/protection.tsx
@@ -0,0 +1,34 @@
+import type { AppProps } from 'next/app'
+import { NextRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { useAppSelector } from 'src/hooks/rtk'
+import { useSyncWithServer } from 'src/hooks/useSyncWithServer'
+import Login from './auth/login'
+
+interface ProtectionProps {
+	router: NextRouter
+	Component: React.ComponentType<AppProps>
+	pageProps: AppProps
+}
+
+const Protection = ({ Component, pageProps }: ProtectionProps) => {
+	const isLoggedIn = useAppSelector(state => state.auth.accessToken)
+	const [loggedIn, setLoggedIn] = useState(false)
+	useSyncWithServer()
+
+	useEffect(() => {
+		if (isLoggedIn) {
+			setLoggedIn(true)
+		} else if (!isLoggedIn) {
+			setLoggedIn(false)
+		}
+	}, [isLoggedIn])
+	return (
+		<>
+			{loggedIn ? <Component {...pageProps} /> : null}
+			{!loggedIn && <Login />}
+		</>
+	)
+}
+
+export default Protection
