ec6a5fa5a0504afa0e635c0c7cc6005967123de6
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-15 02:54:12 +0500

    add lease page

---
+++ src/pages/property/lease.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useState } from 'react'
+import cardImage from 'src/assets/card/pexels-binyamin-mellish-1500459 1.png'
+import { Container } from 'src/components/app/container'
+import { AppFilter } from 'src/components/app/filter'
+import { AppHeader } from 'src/components/app/header'
+import { AppLayout } from 'src/components/app/layout'
+import PropertyBoxCard from 'src/components/property/box-card'
+import { PropertyListCard } from 'src/components/property/list-card'
+import { ListHeader } from 'src/components/property/list-header'
+import propertyService from 'src/services/property'
+import { FilterParameter, Property } from 'src/types/typings'
+import { ApplyPropertyFilter } from 'src/utils/filter'
+
+const listData = [
+	{
+		image: cardImage,
+		contract: 'For Sale',
+		title: 'Zarai Zameen',
+		location: '110 Street 8',
+		category: 'House',
+		occupancy: 'Sold'
+	},
+	{
+		image: cardImage,
+		contract: 'For Sale',
+		title: 'Zarai Zameen',
+		location: '110 Street 8',
+		category: 'House',
+		occupancy: 'Sold'
+	},
+	{
+		image: cardImage,
+		contract: 'For Sale',
+		title: 'Zarai Zameen',
+		location: '110 Street 8',
+		category: 'House',
+		occupancy: 'Sold'
+	},
+	{
+		image: cardImage,
+		contract: 'For Sale',
+		title: 'Zarai Zameen',
+		location: '110 Street 8',
+		category: 'House',
+		occupancy: 'Sold'
+	}
+]
+
+interface PropertyListProps {
+	leasePropertiesData: Property[]
+}
+
+const LeasePropertyList = ({ leasePropertiesData }: PropertyListProps) => {
+	const [filterParams, setFilterParams] = useState<FilterParameter>({
+		period: 'newest',
+		city: '',
+		type: '',
+		category: '',
+		contract: ''
+	})
+
+	const [view, setView] = useState('box')
+	const [data, setData] = useState(leasePropertiesData)
+
+	useEffect(() => {
+		const filteredData = ApplyPropertyFilter(filterParams, leasePropertiesData)
+		setData(filteredData)
+	}, [filterParams])
+
+	return (
+		<AppLayout>
+			<AppHeader />
+			<Container>
+				<ListHeader heading="On Lease (Rental)" count={data.length} setView={setView} />
+				<AppFilter showStatus={true} setFilterData={setFilterParams} />
+				{view === 'box' ? <BoxView data={data} /> : <ListView data={data} />}
+			</Container>
+		</AppLayout>
+	)
+}
+
+interface ViewProps {
+	data: Property[]
+}
+
+const BoxView = ({ data }: ViewProps) => {
+	return (
+		<div className="grid grid-cols-3 gap-x-4 gap-y-3">
+			{data.map((item, index) => (
+				<PropertyBoxCard
+					key={item.Title + index}
+					image={item.PropertyDetails?.images}
+					contract={item.ContractType}
+					title={item.Title}
+					location={item.Location}
+					category={item.PropertyCategory}
+					occupancy={item.AddHistory?.OccupancyStatus}
+					expiryDate={item.AddHistory?.LeaseExpiringOn}
+				/>
+			))}
+		</div>
+	)
+}
+
+const ListView = ({ data }: ViewProps) => <PropertyListCard data={data} />
+
+export const getStaticProps = async () => {
+	const response = await propertyService.getLeaseProperties()
+
+	return {
+		props: {
+			leasePropertiesData: response
+		}
+	}
+}
+
+export default LeasePropertyList
