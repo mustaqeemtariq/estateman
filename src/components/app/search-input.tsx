ed7305ff682ae502d2096774698e408236f5c5ba
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-15 02:48:43 +0500

    refactor code

--- src/components/app/search-input.tsx
+++ src/components/app/search-input.tsx
@@ -1,12 +1,20 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
+import { InputHTMLAttributes } from 'react'
+import { UseFormRegister } from 'react-hook-form'
 
-type SearchInputProps = React.DetailedHTMLProps<
-	React.InputHTMLAttributes<HTMLInputElement>,
-	HTMLInputElement
->
+interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {
+	register?: UseFormRegister<any>
+	name?: string
+}
 
-export const Search = ({ className, placeholder, ...restProp }: SearchInputProps) => {
+export const Search = ({
+	register,
+	name = 'title',
+	className,
+	placeholder,
+	...restProp
+}: SearchInputProps) => {
 	return (
 		<div>
 			<div className="relative rounded-md">
@@ -18,8 +26,8 @@ export const Search = ({ className, placeholder, ...restProp }: SearchInputProps
 				</div>
 				<input
 					type="search"
+					{...(register?.(name) ?? {})}
 					{...restProp}
-					name="search"
 					id="search"
 					className={clsx(
 						'block rounded-md pl-10 p-[15px] outline-none border-slate-300 border text-base sm:w-[380px]',
