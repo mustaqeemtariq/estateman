9f3b71e5a635d1ef65a7e3c0188ba5e8e038661f
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-15 02:48:32 +0500

    design image slider component

---
+++ src/components/app/image-slider.tsx
@@ -0,0 +1,75 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
+import clsx from 'clsx'
+import Image, { StaticImageData } from 'next/image'
+import { useState } from 'react'
+
+interface ImageSliderProps {
+	images: StaticImageData[]
+	type: string
+}
+
+export const ImageSlider = ({ images, type }: ImageSliderProps) => {
+	const [currentIndex, setCurrentIndex] = useState(0)
+
+	const handleSlider = (index: number) => {
+		setCurrentIndex(index)
+	}
+
+	const visibleImages = images.slice(currentIndex, currentIndex + 5)
+
+	return (
+		<div className="relative flex items-center">
+			{type === 'slider' && (
+				<Image src={images[currentIndex]} alt="slider" className="h-60 w-full" />
+			)}
+			<div className="flex items-center space-x-1">
+				{type === 'arrow' && (
+					<div className="justify-start">
+						<ChevronLeftIcon
+							onClick={() => setCurrentIndex(currentIndex > 0 ? currentIndex - 1 : currentIndex)}
+							className={clsx(
+								'h-6 w-6 fill-[#717B9D] opacity-0',
+								currentIndex > 0 && 'opacity-100 cursor-pointer'
+							)}
+							aria-hidden="true"
+						/>
+					</div>
+				)}
+
+				<div className="flex space-x-2 justify-center ">
+					{type === 'arrow' &&
+						visibleImages.map(image => (
+							<Image src={image} alt="card-image" className="h-20 w-20 " />
+						))}
+				</div>
+				{type === 'arrow' && (
+					<ChevronRightIcon
+						onClick={() =>
+							setCurrentIndex(
+								currentIndex + 5 < images.length - 1 ? currentIndex + 1 : currentIndex
+							)
+						}
+						className={clsx(
+							'h-6 w-6 fill-[#717B9D] opacity-0',
+							currentIndex + 5 < images.length - 1 && 'opacity-100 cursor-pointer'
+						)}
+						aria-hidden="true"
+					/>
+				)}
+			</div>
+			{type === 'slider' && (
+				<div className="bg-[#0D0C18]/[75%] w-fit absolute rounded-[30px] flex items-center space-x-2 px-4 py-2 mx-auto bottom-2">
+					{images.map((image, index) => (
+						<div
+							key={index}
+							className={clsx(
+								'h-3 w-3 rounded-full bg-[#D9D9D9]/[75%] cursor-pointer',
+								index === currentIndex && 'bg-[#D9D9D9]'
+							)}
+							onClick={() => handleSlider(index)}></div>
+					))}
+				</div>
+			)}
+		</div>
+	)
+}
