560038ed91dc4985d820a6f97a5509a9c8723c17
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-15 02:54:48 +0500

    add edit page

--- src/pages/property/edit/[title].tsx
+++ src/pages/property/edit/[title].tsx
@@ -6,20 +6,31 @@ import { AppLayout } from 'src/components/app/layout'
 import PropertyForm from 'src/components/property/form'
 import { PropertyHeader } from 'src/components/property/header'
 import propertyService from 'src/services/property'
+import { Property } from 'src/types/typings'
 
-const UpdateProperty = () => {
+interface UpdatePropertyProps {
+	propertyData: Property
+}
+
+const UpdateProperty = ({ propertyData }: UpdatePropertyProps) => {
 	const [state, setState] = useState('Add Property')
 	const [active, setActive] = useState({
-		propertyDetails: false,
-		addHistory: false
+		propertyDetails: true,
+		addHistory: true,
+		allHistory: true
 	})
 
 	return (
 		<AppLayout>
 			<AppHeader />
 			<Container>
-				<PropertyHeader setState={setState} active={active} state={state} />
-				<PropertyForm currentTab={state} setCurrentTab={setState} setActive={setActive} />
+				<PropertyHeader showHistory={true} setState={setState} active={active} state={state} />
+				<PropertyForm
+					data={propertyData}
+					currentTab={state}
+					setCurrentTab={setState}
+					setActive={setActive}
+				/>
 			</Container>
 		</AppLayout>
 	)
@@ -30,7 +41,7 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
 	const response = await propertyService.searchProperty(typeof title == 'string' ? title : '')
 	return {
 		props: {
-			property: response
+			propertyData: response
 		}
 	}
 }
