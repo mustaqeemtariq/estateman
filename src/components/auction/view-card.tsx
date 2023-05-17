faa33be87b2721320726463c0b9a1b0a4fb56ad3
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-17 09:31:29 +0500

    integrate with backend

--- src/components/auction/view-card.tsx
+++ src/components/auction/view-card.tsx
@@ -9,8 +9,14 @@ import Money from 'src/assets/view/Dollar Place Marker.png'
 import Place from 'src/assets/view/Place Marker.png'
 import Phone from 'src/assets/view/iPhone X.png'
 import { ImageSlider } from '../app/image-slider'
+import { Auction } from 'src/types/typings'
+import moment from 'moment'
 
-const ViewAuctionCard = () => {
+interface ViewAuctionCardProps {
+	data: Auction[]
+}
+
+const ViewAuctionCard = ({data}: ViewAuctionCardProps) => {
 	const images = [Image1, EmptyImage, Image1, EmptyImage, Image1, EmptyImage]
 	return (
 		<div className="grid grid-cols-2 gap-x-6 gap-y-3">
@@ -18,17 +24,17 @@ const ViewAuctionCard = () => {
 				<div className="flex justify-between">
 					<div className="space-y-1">
 						<p className="text-xs text-[#CED1DC]">Title</p>
-						<p className="whitespace-nowrap text-semibold">500 sq yd phase 2</p>
+						<p className="whitespace-nowrap text-semibold">{data[0].Title}</p>
 					</div>
 					<div className="space-y-1">
 						<p className="text-xs text-[#CED1DC]">Society</p>
-						<p className="text-semibold">DHA</p>
+						<p className="text-semibold">{data[0].Society}</p>
 					</div>
 				</div>
 				<div className="flex justify-between">
 					<div className="flex items-center space-x-1 text-base">
 						<MapPinIcon className="h-4 w-4 fill-[#717B9D]" aria-hidden="true" />
-						<p className="text-[#0D0C18]">110, Street 8, DHA 2</p>
+						<p className="text-[#0D0C18]">{data[0].Location}</p>
 					</div>
 					<div className="flex items-center space-x-1 text-base">
 						<BiCurrentLocation className="h-4 w-4" />
@@ -37,24 +43,24 @@ const ViewAuctionCard = () => {
 				</div>
 				<div className="flex items-center space-x-1 text-base">
 					<Image src={Area} alt="area" />
-					<p>1000 Sqft</p>
+					<p>{data[0].LandArea}</p>
 				</div>
 				<div className="flex items-center space-x-1 text-base">
 					<CalendarIcon className="h-4 w-4 fill-[#DC4200]" aria-hidden="true" />
 					<p>
-						Auction On: <span className="text-[#DC4200]">March 08, 2023 02:30 PM</span>
+						Auction On: <span className="text-[#DC4200]">{moment(data[0].AuctionDateandTime).format('MMMM DD, YYYY h:mm A')}</span>
 					</p>
 				</div>
 				<div className="flex items-center space-x-1 text-base">
 					<ExclamationTriangleIcon className="h-4 w-4 fill-[#717B9D]" aria-hidden="true" />
 					<p className="text-[#717B9D]">
-						Auctioneer: <span className="text-black">Govt</span>
+						Auctioneer: <span className="text-black">{data[0].Auctioneer}</span>
 					</p>
 				</div>
 				<div className="flex items-center space-x-1 text-base">
 					<Image src={Place} alt="place" />
 					<p className="text-[#717B9D]">
-						Auction: <span className="text-black">On Site</span>
+						Auction: <span className="text-black">{data[0].PlaceofAuction}</span>
 					</p>
 				</div>
 				<div className="flex justify-center space-x-28">
@@ -62,14 +68,14 @@ const ViewAuctionCard = () => {
 						<p className="text-[#717B9D]">Outstanding (Pkr)</p>
 						<div className="flex items-center space-x-1">
 							<Image src={Money} alt="money" />
-							<p className="whitespace-nowrap text-semibold">10,000,000</p>
+							<p className="whitespace-nowrap text-semibold">{data[0].Balance}</p>
 						</div>
 					</div>
 					<div className="space-y-1">
 						<p className="text-[#717B9D]">Reserve Price (Pkr)</p>
 						<div className="flex items-center space-x-1">
 							<Image src={Money} alt="money" />
-							<p className="whitespace-nowrap text-semibold">96,000000</p>
+							<p className="whitespace-nowrap text-semibold">{data[0].ReservePrice}</p>
 						</div>
 					</div>
 				</div>
@@ -77,11 +83,11 @@ const ViewAuctionCard = () => {
 					<div className="flex flex-col space-y-2">
 						<div className="flex items-center space-x-1">
 							<Image src={Contact} alt="contact" />
-							<p>Ahmad Ali</p>
+							<p>{data[0].ContactPerson}</p>
 						</div>
 						<div className="flex items-center space-x-1">
 							<Image src={Phone} alt="phone" />
-							<p className="text-[#0038FF]">0300 7654321</p>
+							<p className="text-[#0038FF]">{data[0].ContactNumber}</p>
 						</div>
 					</div>
 				</div>
