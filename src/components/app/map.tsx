651bb4c28390a0ed0a1182f4f652f8b08b39f9db
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-20 01:19:12 +0500

    map default location changed

--- src/components/app/map.tsx
+++ src/components/app/map.tsx
@@ -4,49 +4,42 @@ import GoogleMapReact from 'google-map-react'
 import { Dispatch, SetStateAction, useState } from 'react'
 
 interface MapComponentProps {
-  onChange?: (lat: number, lng: number) => void
-  setShow: Dispatch<SetStateAction<boolean>>
-  show: boolean
-  lat?: number
-  lng?: number
+	onChange?: (lat: number, lng: number) => void
+	setShow: Dispatch<SetStateAction<boolean>>
+	show: boolean
+	lat?: number
+	lng?: number
 }
 
 interface MarkerProps {
-  lat: number
-  lng: number
+	lat: number
+	lng: number
 }
 
 const Marker = ({ lat, lng }: MarkerProps) => (
-  <MapPinIcon className='h-5 w-5 fill-red-500' aria-hidden='true' />
+	<MapPinIcon className="h-5 w-5 fill-red-500" aria-hidden="true" />
 )
 
-export const MapComponent = ({
-  show,
-  setShow,
-  onChange,
-  lat,
-  lng
-}: MapComponentProps) => {
-  const [location, setLocation] = useState({ lat: lat ?? 0, lng: lng ?? 0 })
+export const MapComponent = ({ show, setShow, onChange, lat, lng }: MapComponentProps) => {
+	const [location, setLocation] = useState({ lat: 0, lng: 0 })
 
-  const handleMapClick = (event: any) => {
-    const { lat, lng } = event
-    setLocation({ lat, lng })
-    onChange?.(lat, lng)
-    setShow(false)
-  }
+	const handleMapClick = (event: any) => {
+		const { lat, lng } = event
+		setLocation({ lat, lng })
+		onChange?.(lat, lng)
+		setShow(false)
+	}
 
-  return (
-    <div className={clsx(!show && 'hidden')} style={{ height: '400px', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyCbOobz-eShsFo7BaB4BkwOafME7TC2vCc' }}
-        yesIWantToUseGoogleMapApiInternals
-        defaultCenter={{ lat: 37.7749, lng: -122.4194 }}
-        defaultZoom={12}
-        onClick={handleMapClick}
-      >
-        <Marker lat={location.lat} lng={location.lng} />
-      </GoogleMapReact>
-    </div>
-  )
+	return (
+		<div className={clsx(!show && 'hidden')} style={{ height: '400px', width: '100%' }}>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: 'AIzaSyCbOobz-eShsFo7BaB4BkwOafME7TC2vCc' }}
+				yesIWantToUseGoogleMapApiInternals
+				defaultCenter={{ lat: lat ?? 33.6844, lng: lng ?? 73.0479 }}
+				defaultZoom={6}
+				onClick={handleMapClick}>
+				<Marker lat={location.lat} lng={location.lng} />
+			</GoogleMapReact>
+		</div>
+	)
 }
