004983814e9e1c08ea5472bb482f1843bdd64d1c
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-05-20 01:21:45 +0500

    change edit page

---
+++ src/pages/property/edit.tsx
@@ -0,0 +1,56 @@
+import { GetServerSidePropsContext } from 'next'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { Container } from 'src/components/app/container'
+import { AppHeader } from 'src/components/app/header'
+import { AppLayout } from 'src/components/app/layout'
+import PropertyForm from 'src/components/property/form'
+import { PropertyHeader } from 'src/components/property/header'
+import propertyService from 'src/services/property'
+import { Property } from 'src/types/typings'
+
+interface UpdatePropertyProps {
+	propertyData: Property
+}
+
+const UpdateProperty = ({ propertyData }: UpdatePropertyProps) => {
+	const router = useRouter()
+	const { tab } = router.query
+	console.log('ssssssssss', tab)
+
+	const [state, setState] = useState((tab as string) ?? 'Add Property')
+	const [active, setActive] = useState({
+		propertyDetails: true,
+		addHistory: true,
+		allHistory: true
+	})
+
+	return (
+		<AppLayout>
+			<AppHeader />
+			<Container>
+				<PropertyHeader showHistory={true} setState={setState} active={active} state={state} />
+				<PropertyForm
+					editData={propertyData}
+					isNew={false}
+					currentTab={state}
+					setCurrentTab={setState}
+					setActive={setActive}
+				/>
+			</Container>
+		</AppLayout>
+	)
+}
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+	const { title } = ctx.query
+
+	const response = await propertyService.searchProperty(typeof title == 'string' ? title : '')
+	return {
+		props: {
+			propertyData: response
+		}
+	}
+}
+
+export default UpdateProperty
