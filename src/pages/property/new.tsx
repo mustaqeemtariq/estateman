f39fa4f1c702ed4294588e35efb649d9771ca8c1
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-06-19 00:12:44 +0500

    remove unneeded state

--- src/pages/property/new.tsx
+++ src/pages/property/new.tsx
@@ -2,28 +2,24 @@ import { useState } from 'react'
 import { Container } from 'src/components/app/container'
 import { AppHeader } from 'src/components/app/header'
 import { AppLayout } from 'src/components/app/layout'
-import PropertyForm from 'src/components/property/form'
+import AddHistoryForm from 'src/components/property/add-history'
+import AddPropertyForm from 'src/components/property/add-property'
 import { PropertyHeader } from 'src/components/property/header'
+import PropertyDetailsForm from 'src/components/property/property-details'
 
 const newProperty = () => {
-	const [state, setState] = useState('Add Property')
-	const [active, setActive] = useState({
-		propertyDetails: false,
-		addHistory: false,
-		allHistory: false
-	})
+	const [state, setState] = useState('Property Details')
+	const [propertyId, setPropertyId] = useState('')
+	const [category, setCategory] = useState<string>('')
 
 	return (
 		<AppLayout>
 			<AppHeader />
 			<Container>
-				<PropertyHeader setState={setState} active={active} state={state} />
-				<PropertyForm
-					isNew={true}
-					currentTab={state}
-					setCurrentTab={setState}
-					setActive={setActive}
-				/>
+				<PropertyHeader setState={setState} isNew={true} state={state} />
+				{state === 'Add Property' && <AddPropertyForm setCategory={setCategory} setId={setPropertyId} setCurrentTab={setState} />}
+				{state === 'Property Details' && <PropertyDetailsForm category={category} propertyId={propertyId} setCurrentTab={setState} />}
+				{state === 'Add History' && <AddHistoryForm propertyId={propertyId} />}
 			</Container>
 		</AppLayout>
 	)
